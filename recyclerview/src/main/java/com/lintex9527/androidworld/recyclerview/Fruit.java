8c57e689ce39044bbb166ae431a44d820f29ebdc
LinTeX9527 <LinTeX9527@yeah.net>
2018-09-02 17:28:43 +0800

    添加水果类和显示单个水果的布局

---
+++ recyclerview/src/main/java/com/lintex9527/androidworld/recyclerview/Fruit.java
@@ -0,0 +1,25 @@
+package com.lintex9527.androidworld.recyclerview;
+
+/**
+ * Created by LinTeX9527 on 2018/9/2.
+ */
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
--- recyclerview/src/main/java/com/lintex9527/androidworld/recyclerview/MainActivity.java
+++ recyclerview/src/main/java/com/lintex9527/androidworld/recyclerview/MainActivity.java
@@ -3,6 +3,12 @@ package com.lintex9527.androidworld.recyclerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * 注意RecyclerView控件是在 com.android.support:recyclerview-v7中的
+ * 需要在当前模块的build.gradle中的dependencies闭包中添加依赖。
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
